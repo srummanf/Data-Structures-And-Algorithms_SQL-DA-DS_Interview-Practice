11f57e2fa0e797b47e712e8f10d55774756bf5de
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2026-06-22 12:08:10 +0530

    Time: 2 ms (96.67%) | Memory: 43.3 MB (71.44%) - LeetSync

---
+++ LeetcodeAutomated/1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[26];
+
+        for (char ch : text.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        int count = 0;
+
+        while (freq['b' - 'a'] >= 1 &&
+               freq['a' - 'a'] >= 1 &&
+               freq['l' - 'a'] >= 2 &&
+               freq['o' - 'a'] >= 2 &&
+               freq['n' - 'a'] >= 1) {
+
+            freq['b' - 'a']--;
+            freq['a' - 'a']--;
+            freq['l' - 'a'] -= 2;
+            freq['o' - 'a'] -= 2;
+            freq['n' - 'a']--;
+
+            count++;
+        }
+
+        return count;
+    }
+}
