43694419a157e22552570f2103a74ff093facd2c
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2026-06-22 12:23:59 +0530

    Time: 2 ms (96.67%) | Memory: 43.6 MB (50.04%) - LeetSync

--- LeetcodeAutomated/1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ LeetcodeAutomated/1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -6,23 +6,39 @@ class Solution {
             freq[ch - 'a']++;
         }
 
-        int count = 0;
-
-        while (freq['b' - 'a'] >= 1 &&
-               freq['a' - 'a'] >= 1 &&
-               freq['l' - 'a'] >= 2 &&
-               freq['o' - 'a'] >= 2 &&
-               freq['n' - 'a'] >= 1) {
-
-            freq['b' - 'a']--;
-            freq['a' - 'a']--;
-            freq['l' - 'a'] -= 2;
-            freq['o' - 'a'] -= 2;
-            freq['n' - 'a']--;
-
-            count++;
-        }
-
-        return count;
+        return Math.min(
+                Math.min(freq['b' - 'a'], freq['a' - 'a']),
+                Math.min(
+                        Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                        freq['n' - 'a']));
     }
 }
+
+// class Solution {
+//     public int maxNumberOfBalloons(String text) {
+//         int[] freq = new int[26];
+
+//         for (char ch : text.toCharArray()) {
+//             freq[ch - 'a']++;
+//         }
+
+//         int count = 0;
+
+//         while (freq['b' - 'a'] >= 1 &&
+//                freq['a' - 'a'] >= 1 &&
+//                freq['l' - 'a'] >= 2 &&
+//                freq['o' - 'a'] >= 2 &&
+//                freq['n' - 'a'] >= 1) {
+
+//             freq['b' - 'a']--;
+//             freq['a' - 'a']--;
+//             freq['l' - 'a'] -= 2;
+//             freq['o' - 'a'] -= 2;
+//             freq['n' - 'a']--;
+
+//             count++;
+//         }
+
+//         return count;
+//     }
+// }
