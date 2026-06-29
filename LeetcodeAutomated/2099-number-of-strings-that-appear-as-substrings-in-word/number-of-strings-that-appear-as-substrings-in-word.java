b6aec6eb9eb99447c8a55ff0d5c7f5a3e120300f
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2026-06-29 12:49:40 +0530

    Time: 1 ms (75.75%) | Memory: 43.6 MB (19.94%) - LeetSync

--- LeetcodeAutomated/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ LeetcodeAutomated/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,20 +1,34 @@
 class Solution {
-
-    boolean doesContain(String pat, String word){
-        int len = word.length();
-        for(int i= 0; i<len; i++){
-            for(int j=i; j<len+1; j++){
-                String ss = word.substring(i, j);
-                if(ss.equals(pat))return true;
-            }
-        }
-        return false;
-    }
     public int numOfStrings(String[] patterns, String word) {
-        int count = 0;
+        int count =0;
         for(String w: patterns){
-            if(doesContain(w, word)) count++;
+            if(word.contains(w)){
+                count++;
+
+            }
+
         }
         return count;
     }
 }
+
+
+// Brute Force : O(n^2)
+
+//  boolean doesContain(String pat, String word){
+//         int len = word.length();
+//         for(int i= 0; i<len; i++){
+//             for(int j=i; j<len+1; j++){
+//                 String ss = word.substring(i, j);
+//                 if(ss.equals(pat))return true;
+//             }
+//         }
+//         return false;
+//     }
+//     public int numOfStrings(String[] patterns, String word) {
+//         int count = 0;
+//         for(String w: patterns){
+//             if(doesContain(w, word)) count++;
+//         }
+//         return count;
+//     }
