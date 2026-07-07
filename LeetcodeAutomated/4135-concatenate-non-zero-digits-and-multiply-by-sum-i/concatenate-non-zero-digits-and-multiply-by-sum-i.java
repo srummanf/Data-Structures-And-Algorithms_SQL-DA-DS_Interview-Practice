b64c4e818772b055df0c431ae6e31d391f1d4a17
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2026-07-07 10:43:34 +0530

    Time: 4 ms (11.06%) | Memory: 43 MB (7.32%) - LeetSync

--- LeetcodeAutomated/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ LeetcodeAutomated/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -11,17 +11,19 @@ class Solution {
 
     long nonZeroDigits(int n) {
         String num = Integer.toString(n);
-        if(n==0) return 0;
+
         String finalNum = "";
         for (char ch : num.toCharArray()) {
             if (ch != '0')
                 finalNum += String.valueOf(ch);
         }
 
-        return finalNum=="0"? 0 :Long.parseLong(finalNum);
+        return Long.parseLong(finalNum);
     }
 
     public long sumAndMultiply(int n) {
+        if (n == 0)
+            return 0;
         return sumOfDigits(n) * nonZeroDigits(n);
     }
 }
