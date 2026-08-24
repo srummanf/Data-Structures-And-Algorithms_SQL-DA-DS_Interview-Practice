19042f33de8400d3136ae2c7ad8466e61b740359
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2026-08-24 11:40:01 +0530

    Time: 28 ms (6.60%) | Memory: 52.4 MB (22.28%) - LeetSync

---
+++ LeetcodeAutomated/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -0,0 +1,31 @@
+class KthLargest {
+
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        this.pq = new PriorityQueue<>();
+
+        for (int n : nums) {
+
+            pq.add(n);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+
+        pq.add(val);
+
+        if (pq.size() > k) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}
